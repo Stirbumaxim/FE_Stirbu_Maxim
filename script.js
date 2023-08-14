274dfd4ced75dfee2e244b4ab6bd1c5a8f6a01a3
Stirbumaxim <133904888+Stirbumaxim@users.noreply.github.com>
2023-08-14 08:18:38 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,56 @@
+// console.log("Homework_Maxim_Stirbu_160523evening");
+
+// -------------------------------------------
+
+// let d = 7;
+// console.log(d);
+// console.log(d*d);
+// console.log(d*d*d);
+
+// ------------------------------------------------
+
+// let num1 = Number(prompt("Enter your num"));
+// if (num1>0) {
+//     alert(num1 *2);
+// }
+// else{
+//     alert("Your number is not positive");
+// }
+
+//-------------------------------------------------
+
+// let a = Number(prompt("Enter your firt number"));
+// let b = Number(prompt("Enter your second number"));
+
+// if (a>b) {
+//     alert(a+b);
+// } 
+// if (a===b ) {
+//     alert("Numbers are identical");
+// }
+// else{
+//     alert(a*b);
+// }
+
+// --------------------------------------------------
+
+// let a = Number(prompt("Enter your number"));
+// if (a<0) {
+// alert(a*a);
+// } else {
+//     alert("Error");
+// }
+
+// ---------------------------------------------------
+
+// let a = Number(prompt("How many stundent are in class?"));
+// let b = Number(prompt("How many chairs are in class?"));
+// if (a<=b) {
+//     alert("Everybody have a chair");
+// } else {
+//     alert("There are not enough chairs");
+// }
+
+// ----------------------------------------------------
+    
+
