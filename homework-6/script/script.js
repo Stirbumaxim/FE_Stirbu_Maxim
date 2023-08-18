0bbc7e377279671631b966ead677dd7924730842
Stirbumaxim <133904888+Stirbumaxim@users.noreply.github.com>
2023-08-18 12:58:16 +0200

    Add files via upload

---
+++ homework-6/script/script.js
@@ -0,0 +1,58 @@
+// let user = {
+//     name: "John",
+//     age: 30
+//     };
+
+//     console.log(user.age);
+
+// let product ={
+//      productname: 'Ananas' ,
+//     productprice: 10,
+//     }
+
+//     console.log(product);
+
+// product.quantity = 30,
+// product.quality = "good"
+
+// delete product.quality;
+
+// if (condition) {
+    
+// }
+
+// for (let key in user) {
+//    if (typeof user[key]  === Number) {
+//     user[key] *= 2
+//    }
+// }
+
+// for (let key in product) {
+//     if (typeof product[key]  === Number) {
+//      product[key] *= 2
+//     }
+//  }
+
+// function compare(a,b) {
+//     if (a>b) {
+//         return 1;
+//     }
+
+//    else if ( a < b){
+//         return -1;
+//    } 
+   
+//    else{
+//     return 0;
+//    }  
+// }
+
+// function process(num1, num2) {
+//     if (num1 % 2 === 0 && num2 % 2 === 0) {
+//       return num1 * num2;
+//     } else if (num1 % 2 !== 0 && num2 % 2 !== 0) {
+//       return num1 + num2;
+//     } else {
+//       return num1 % 2 !== 0 || num2 % 2 ==0;
+//     }
+//   }
